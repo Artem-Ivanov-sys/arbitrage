a07c662529c38b82b2bb0aebdda33ef446896577
alex-HAMSTER <119113632+alex-HAMSTER@users.noreply.github.com>
2025-07-22 03:39:20 +0300

    окуратно, тут изменения потужні. Style green, red,

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -78,7 +78,7 @@ function createRow(data) {
           <p>${data['long']['rate'].toFixed(4)}% <small style="margin-left: 5px ;"> ${time[data['long']['exchange']]}h</small> </p>
       </td>
       <td class="coin-name" rowspan="2">${Math.abs((data['long']['rate'] * (24 / time[data['long']['exchange']]) ) - (data['short']['rate'] * (24 / time[data['short']['exchange']]) ) * 8760).toFixed(4)}%</td>
-      <td class="coin-name" rowspan="2"><span class="badge green">${((data['long']['rate'] - data['short']['rate'])*100).toFixed(4)}%</span></td>
+      <td class="coin-name" rowspan="2"><span class="badge ${((data['long']['rate'] - data['short']['rate']) >= 0 ? 'green' : 'red')}">   ${((data['long']['rate'] - data['short']['rate'])*100).toFixed(4)}% </span></td>
       <td class="coin-name" rowspan="2"><span class="badge green">${data['short']['index_price'] && data['long']['index_price'] ? ((data['short']['index_price']-data['long']['index_price'])*2/(data['long']['index_price']+data['short']['index_price'])*100).toFixed(4) : -1}%</span></td>
   </tr>
   <tr class="second_tr">
