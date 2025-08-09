b21cbe4b67bb1b4bf96ea45ca5cfe8d7980172ee
Artem-Ivanov-sys <Art_Iv3@ukr.net>
2025-08-09 03:18:38 +0300

    1.0a-4 nginx fix 4

--- backend/fundings_api/admin.py
+++ backend/fundings_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserModel, SessionModel
 
 # Register your models here.
+
+@admin.register(UserModel)
+class UserModeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'user_subscription_level', 'user_subscription_expire']
+
+@admin.register(SessionModel)
+class SessionModelAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'user_agent', 'ip_address']
